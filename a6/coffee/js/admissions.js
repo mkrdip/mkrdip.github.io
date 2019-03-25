f2fec9f2b27187f4bbf5ecfb5f219b3e49f618d5
Mrinal Kanti Roy <dip@professionalthemes.nyc>
2019-03-24 21:31:05 -0500

    Update admissions.js

--- a6/coffee/js/admissions.js
+++ a6/coffee/js/admissions.js
@@ -143,12 +143,11 @@ function update(rawdata){
                                 }
                                 return color;
                             });   
-                            // Inspiration: https://learnui.design/tools/data-color-picker.html#palette
-                            
-          var yaxisScale = d3.scaleLinear().domain([max, 0]).range([0, height+10]);
-          vis.append('g').attr("transform", "translate(505, 0)").call(d3.axisRight(yaxisScale).ticks(5));                            
+                            // Inspiration: https://learnui.design/tools/data-color-picker.html#palette                            
           var xaxisScale = d3.scaleBand().domain(x).range([0, width+3]);
-          vis.append('g').attr("transform", "translate(0, 310)").call(d3.axisBottom(xaxisScale)); 
+          vis.append('g').attr("transform", "translate(0, 305)").call(d3.axisBottom(xaxisScale));
+          var yaxisScale = d3.scaleLinear().domain([max, 0]).range([0, height+10]);
+          vis.append('g').attr("transform", "translate(503, 0)").call(d3.axisRight(yaxisScale).ticks(5));           
 
 
 }
